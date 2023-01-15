df47bd73b3d843dc492a75ecb0ff93979bca5a22
Mikhail Riazanov <razonmike@gmail.com>
2023-01-15 23:00:47 +0300

    fix2

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,14 +19,8 @@ terraform {
   }
 }
 provider "yandex" {
-
   token      = var.yc_token
   cloud_id   = var.yc_cloud_id
   folder_id  = var.yc_folder_id
   zone       = var.yc_zone     
-provider "yandex" {
-  token     = var.yc_token
-  cloud_id  = var.yc_cloud_id
-  zone      = var.yc_zone
- }
 }
